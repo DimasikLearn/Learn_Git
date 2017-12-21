5b902ca3b9d275159caae7efff5a89d7025bf25a
Dimaso <dimaso@Dimaso.Ishchenko>
2017-12-21 18:47:35 +0200

    Changed auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
 function authenticate(login, password){
-  if (login === 'login' || password === 'password'){
-    return 'You were logged';}
-    else { return 'Login is incorrect!!!!!!!!!!!'
+  if (login === 'login'){
+    return 'You were logged';
+  } else { 
+      return 'Login is correct'
     }
   }
