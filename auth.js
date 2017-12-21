69250884e37b1baa9c52cb78c66f717f80b4700a
Dimaso <dimaso@Dimaso.Ishchenko>
2017-12-21 18:32:08 +0200

    Cheange auth on conflicted branch

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function authenticate(login, password){
-  if (login === 'login' && password === 'password'){
+  if (login === 'login' || password === 'password'){
     return 'You were logged';}
-    else { return 'Login is incorrect'
+    else { return 'Login is incorrect!!!!!!!!!'
     }
   }
